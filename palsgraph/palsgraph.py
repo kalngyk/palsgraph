b5c9a4953ae79c7e06637658205f162d27cc9ff5
Kal <kalngyk@gmail.com>
2022-05-01 21:13:26 +0800

    Bug fix

--- palsgraph/palsgraph.py
+++ palsgraph/palsgraph.py
@@ -2,6 +2,7 @@ import palsgetpos
 import numpy as np
 import networkx as nx
 from seaborn import color_palette
+from colorcet import glasbey
 
 __all__ = ['make_graph', 'getpos', 'gen_colormap']
 
@@ -22,19 +23,34 @@ def make_graph(dist, labels=None, show_singletons=False):
 
 
 def getpos(G, communities):
+    """
+    Find a position for every node in G.nodes according to the clusters specified in communities
+    Every name in communities must exist in G.nodes, but not every node in G.nodes needs to be
+    in communities.
+
+    :param G: A NetworkX graph
+    :param communities: A list of lists
+    :return: An list of the same length of G.nodes. Each element in the list is a position for a node.
+    """
+    elems = [item for sublist in communities for item in sublist]
+    for node in G.nodes:
+        if node not in elems: 
+            raise Exception("Exists node in G not specified in communities") 
+
     node2idx = dict([(x, i) for (i, x) in enumerate(G.nodes)])
     clusters = []
     singletons = []
     for n, c in enumerate(communities):
         if len(c) == 1:
-            singletons.extend([node2idx[x] for x in c])
+            if c[0] in G.nodes:
+                singletons.extend([node2idx[x] for x in c])
         indices = [node2idx[x] for x in G.nodes if x in c]
         for i in indices:
             clusters.append([i, n])
     clusters = np.array(clusters)
     edges = [[node2idx[x], node2idx[y]] for x, y in G.edges]
     edges.extend([[x, x] for x in singletons])
-    edges = np.array(edges)    
+    edges = np.array(edges)
     pos = palsgetpos.getpos(edges, clusters)
     nodes = np.array(G.nodes)
     results = dict([(nodes[int(i)], np.array([j, k])) for i, j, k in pos])
@@ -42,6 +58,21 @@ def getpos(G, communities):
 
 
 def gen_colormap(G, communities):
+    """
+    Generate a color for every node in G.nodes. Nodes in the same community according to communities
+    are given the same color.
+    Every name in communities must exist in G.nodes, but not every node in G.nodes needs to be in
+    communities.
+
+    :param G: A NetworkX graph
+    :param communities: A list of lists
+    :return: An list of the same length of G.nodes. Each element in the list is a color for a node.
+    """
+    elems = [item for sublist in communities for item in sublist]
+    for node in G.nodes:
+        if node not in elems: 
+            raise Exception("Exists node in G not specified in communities") 
+
     color_map = []
     n2c = dict()
     color_count = 0
@@ -53,7 +84,7 @@ def gen_colormap(G, communities):
         else:
             for n in c:
                 n2c[str(n)] = -1
-    palette = color_palette(None, color_count + 1)
+    palette = color_palette(glasbey, color_count + 1)
     for node in G.nodes:
         idx = n2c[str(node)] if n2c[str(node)] >= 0 else color_count 
         color_map.append(palette[idx])
